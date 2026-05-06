893bfef80ae226baf28ebc7970cf96bf82628bad
Med-dev-sl <mohamedsallu24@gmail.com>
2026-05-06 15:30:03 +0000

    feat(middleware): add CSP middleware to enhance security

--- eksms/eksms/settings.py
+++ eksms/eksms/settings.py
@@ -52,6 +52,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'eksms.csp_middleware.CSPMiddleware',
 ]
 
 ROOT_URLCONF = 'eksms.urls'
