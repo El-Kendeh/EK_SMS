5bde7ec92e9abab743f3ef7f58761b8401c22433
Med-dev-sl <mohamedsallu24@gmail.com>
2026-05-07 07:56:54 +0000

    Remove duplicate 0038 merge and add final merge for 0036_rename branch

---
+++ eksms/eksms_core/migrations/0039_merge_20260507_1200.py
@@ -0,0 +1,11 @@
+from django.db import migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('eksms_core', '0036_rename_lc_class_start_idx_eksms_core__classro_d09161_idx_and_more'),
+        ('eksms_core', '0038_merge_20260507_0659'),
+    ]
+
+    operations = []
