a861696fb6c72ee1d248a9255782ca2f3b4ad550
Med-dev-sl <mohamedsallu24@gmail.com>
2026-05-13 11:36:58 +0000

    allowed all headers in cors for production compatibility

--- backend_node/src/index.js
+++ backend_node/src/index.js
@@ -41,7 +41,7 @@ app.use(cors({
   },
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With', 'referrer-policy', 'x-client-id']
+  allowedHeaders: ['*']
 }));
 
 app.use(bodyParser.json());
