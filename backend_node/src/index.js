1b5a2f960d96ef4eab19fb7f34dd11adbe27b733
Med-dev-sl <mohamedsallu24@gmail.com>
2026-05-14 07:36:30 +0000

    fixing CORS

--- backend_node/src/index.js
+++ backend_node/src/index.js
@@ -51,6 +51,21 @@ app.use(cors({
   optionsSuccessStatus: 200
 }));
 
+// Fallback access control headers for any request that reaches the server
+app.use((req, res, next) => {
+  const origin = req.headers.origin;
+  if (origin && isAllowedOrigin(origin)) {
+    res.header('Access-Control-Allow-Origin', origin);
+    res.header('Access-Control-Allow-Credentials', 'true');
+    res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS,PATCH');
+    res.header('Access-Control-Allow-Headers', 'Content-Type,Authorization,X-CSRFToken,X-Requested-With,Accept');
+  }
+  if (req.method === 'OPTIONS') {
+    return res.sendStatus(200);
+  }
+  next();
+});
+
 // Manual preflight handler
 app.options('*', cors());
 
